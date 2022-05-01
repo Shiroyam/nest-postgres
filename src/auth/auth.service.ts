18ad5dba9babf42a7a5d6cd0eedafdf7c0d0d71b
Semyon Zasov <semenmaslina@gmail.com>
2022-05-01 21:45:44 +0300

    add jwt in auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findByCond({
       email,
@@ -14,5 +18,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user: any) {
+    const { password, ...userData } = user;
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
